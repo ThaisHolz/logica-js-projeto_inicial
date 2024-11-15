c76df2f80bad0278b75af53d344a2461be458e87
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-11-15 15:09:03 +0000

    Adicinando dicas ao jogo

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 alert('Bem vindo ao jogo do número secreto!');
 let numeroSecreto = 21;
 console.log(`É o número ${numeroSecreto}`);
-let chute = prompt('Escolha um número entre 1 e 30:');
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute){
-    alert(`Você acertou! É o número ${numeroSecreto}`);
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 30:');
+    if (numeroSecreto == chute){
+        alert(`Você acertou! É o número ${numeroSecreto} com ${tentativas} tentativas.`);
+    }
+    else{
+        if(numeroSecreto > chute){
+            alert('O número é maior que ' + chute); //O + substitui o ${}
+        }
+        else{
+            alert('O número é menor que ' + chute);
+        }
+        tentativas ++; //++ é igual +1
+    }
 }
-else{
-    alert('Tente novamente.');
-}
+
