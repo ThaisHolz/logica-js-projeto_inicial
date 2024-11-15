d353bc9b0903153adc41e4808bd4e3d6c2fdfb44
Thaís Cristine Coelho <tcristine84@gmail.com>
2024-11-15 16:11:20 +0000

    Substituindo o if e else por uma variavel no tentativas

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # logica-js-projeto_inicial
- Estudo inicial de lógica com JS
+
+ Estudo inicial de lógica com JavaScript, para criar um jogo simples de adivinhação de número.
+
+ HTML já existente feito pela equipe Alura.
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let tentativas = 1;
 while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 30:');
     if (numeroSecreto == chute){
-        alert(`Você acertou! É o número ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     }
     else{
         if(numeroSecreto > chute){
@@ -19,4 +19,10 @@ while(chute != numeroSecreto){
         tentativas ++; //++ é igual +1
     }
 }
+
+//Substituição do if e else
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Você acertou! É o número ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
 
